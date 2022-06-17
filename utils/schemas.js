96068a7c14f12963daca4764e029ef19da2ad9cb
CTK WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2022-06-18 01:30:15 +0530

    Update schemas.js

--- utils/schemas.js
+++ utils/schemas.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose")
 const User = new mongoose.Schema({
     id: { type: String, unique: true, required: true },
     wallet: { type: Number, default: 0 },
-    bank: { type: Number, default: 0 }
+    bank: { type: Number, default: 0 },
+    cooldowns: {
+        work: { type: Date },
+        beg: { type: Date },
+        daily: { type: Date }
+    }
 })
 
 module.exports = { User: mongoose.model("User", User) }
